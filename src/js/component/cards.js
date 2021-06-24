739a09cb9f678e50fdf826e2ec78dd7fc980d3f8
minijuan124 <minijuan124@gmail.com>
2021-06-24 15:42:57 -0400

    landing page

---
+++ src/js/component/cards.js
@@ -0,0 +1,80 @@
+import React from "react";
+import { Card, CardGroup, Button } from "react-bootstrap";
+export const Cards = () => {
+	return (
+		<CardGroup>
+			<Card style={{ Width: "15rem" }}>
+				<Card.Img variant="top" src="holder.js100px180" />
+				<Card.Body>
+					<Card.Title>Card Title</Card.Title>
+					<Card.Text>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+						sed do eiusmod tempor incididunt ut labore et dolore
+						magna aliqua. Ut enim ad minim veniam, quis nostrud
+						exercitation ullamco laboris nisi ut aliquip ex ea
+						commodo consequat. Duis aute irure dolor in
+						reprehenderit in voluptate velit esse cillum dolore eu
+						fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+						non proident, sunt in culpa qui officia deserunt mollit
+						anim id est laborum.
+					</Card.Text>
+					<Button variant="primary">GO</Button>
+				</Card.Body>
+			</Card>
+			<Card style={{ Width: "15rem" }}>
+				<Card.Img variant="top" src="holder.js100px180" />
+				<Card.Body>
+					<Card.Title>Card Title</Card.Title>
+					<Card.Text>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+						sed do eiusmod tempor incididunt ut labore et dolore
+						magna aliqua. Ut enim ad minim veniam, quis nostrud
+						exercitation ullamco laboris nisi ut aliquip ex ea
+						commodo consequat. Duis aute irure dolor in
+						reprehenderit in voluptate velit esse cillum dolore eu
+						fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+						non proident, sunt in culpa qui officia deserunt mollit
+						anim id est laborum.
+					</Card.Text>
+					<Button variant="primary">GO</Button>
+				</Card.Body>
+			</Card>
+			<Card style={{ Width: "15rem" }}>
+				<Card.Img variant="top" src="holder.js100px180" />
+				<Card.Body>
+					<Card.Title>Card Title</Card.Title>
+					<Card.Text>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+						sed do eiusmod tempor incididunt ut labore et dolore
+						magna aliqua. Ut enim ad minim veniam, quis nostrud
+						exercitation ullamco laboris nisi ut aliquip ex ea
+						commodo consequat. Duis aute irure dolor in
+						reprehenderit in voluptate velit esse cillum dolore eu
+						fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+						non proident, sunt in culpa qui officia deserunt mollit
+						anim id est laborum.
+					</Card.Text>
+					<Button variant="primary">GO</Button>
+				</Card.Body>
+			</Card>
+			<Card style={{ Width: "15rem" }}>
+				<Card.Img variant="top" src="holder.js100px180" />
+				<Card.Body>
+					<Card.Title>Card Title</Card.Title>
+					<Card.Text>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+						sed do eiusmod tempor incididunt ut labore et dolore
+						magna aliqua. Ut enim ad minim veniam, quis nostrud
+						exercitation ullamco laboris nisi ut aliquip ex ea
+						commodo consequat. Duis aute irure dolor in
+						reprehenderit in voluptate velit esse cillum dolore eu
+						fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+						non proident, sunt in culpa qui officia deserunt mollit
+						anim id est laborum.
+					</Card.Text>
+					<Button variant="primary">GO</Button>
+				</Card.Body>
+			</Card>
+		</CardGroup>
+	);
+};
